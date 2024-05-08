466b41f83a76f211351b2d3d8907932bef6cfb2f
Eric VanderHelm <vanderhelm.eric@gmail.com>
2024-05-07 23:21:09 -0500

    add vite+react+cesium

---
+++ app/src/App.tsx
@@ -0,0 +1,20 @@
+import './App.css'
+
+import { Viewer, Entity, PointGraphics } from "resium";
+import { Cartesian3 } from "cesium";
+
+const position = Cartesian3.fromDegrees(-74.0707383, 40.7117244, 100);
+
+function App() {
+  return (
+    <Viewer
+      full
+    >
+      <Entity position={position} name="Tokyo" description="Hello, world.">
+        <PointGraphics pixelSize={10} />
+      </Entity>
+    </Viewer>
+  )
+}
+
+export default App
