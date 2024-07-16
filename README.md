28148f6c465c3f7cb8155e728201a90f20eeb313
Eric VanderHelm <vanderhelm.eric@gmail.com>
2024-07-16 14:33:07 -0500

    switch from leaflet to mapbox

--- README.md
+++ README.md
@@ -3,3 +3,17 @@
 [demo](https://gfsweather.com)
 
 ![demo](https://github.com/evanderh/gfsweather/assets/3112477/17e4dce5-767b-4d1f-bc99-72910bd57830)
+
+# build
+
+build frontend production image
+
+```
+docker build -t gfsweather-frontend-prod -f frontend/Dockerfile.prod frontend/
+```
+
+run frontend prod image
+
+```
+docker run --rm -it -p 80:80 -v ./layers:/usr/share/nginx/html/layers gfsweather-frontend-prod
+```
--- frontend/main.js
+++ frontend/main.js
@@ -1,19 +1,21 @@
 import './style.css'
-import 'leaflet/dist/leaflet.css';
-import 'leaflet-velocity/dist/leaflet-velocity.css';
-import 'leaflet-timedimension/dist/leaflet.timedimension.control.css';
+import 'mapbox-gl/dist/mapbox-gl.css';
 
-import L from 'leaflet';
-import 'leaflet-velocity/dist/leaflet-velocity.js';
-import 'leaflet-timedimension/dist/leaflet.timedimension.src.js';
-import './src/TimeLayer.js';
-import './src/TimeVelocityLayer.js';
-import './src/legend.js';
-import { layers } from './src/layers.js';
+import mapboxgl from 'mapbox-gl'; // or "const mapboxgl = require('mapbox-gl');"
 
-const hoursPerForecast = 3;
+mapboxgl.accessToken = 'pk.eyJ1IjoiZXZhbmRlcmgiLCJhIjoiY2x5bnM3bzduMDlzZTJrcHRncXI4MWNiYyJ9.5IREPfoU_ulCHL0Oxjxq1w';
 const SERVER_URL = import.meta.env.VITE_SERVER_URL;
 const TILES_URL = import.meta.env.VITE_TILES_URL;
+
+const map = new mapboxgl.Map({
+    container: 'map', // container ID
+    center: [-74.5, 40], // starting position [lng, lat]. Note that lat must be set between -90 and 90
+    zoom: 3,
+    minZoom: 3,
+    maxZoom: 6,
+});
+
+const hoursPerForecast = 3;
 
 async function fetchData() {
     try {
@@ -29,76 +31,27 @@ async function fetchData() {
     }
 }
 
-let map;
-
 function render(startDatetime, numForecasts) {
     let hourLimit = (numForecasts - 1) * hoursPerForecast;
     let start = (new Date(startDatetime)).toISOString()
-                                             .substring(0, 13);
+                                         .substring(0, 13);
 
-    if (map) {
-        map.off();
-        map.remove()
-    }
-    
-    map = L.map('map', {
-        center: [20, -10],
-        zoom: 3,
-        minZoom: 3,
-        maxZoom: 6,
-        zoomSnap: 0.5,
-        zoomDelta: 1,
-        maxBounds: [[-85, -720], [85, 720]],
-        timeDimension: true,
-        timeDimensionOptions: {
-            timeInterval: `${startDatetime}/PT${hourLimit}H`,
-            period: `PT${hoursPerForecast}H`,
-        },
-        timeDimensionControl: true,
-        timeDimensionControlOptions: {
-            playerOptions: {
-                transitionTime: 2000,
-            },
-            timeZones: ["Local"],
-            playButton: true,
-            loopButton: true,
-            speedSlider: false,
-        },
+    map.addSource('custom-tiles', {
+        type: 'raster',
+        tiles: [
+            `${SERVER_URL}/layers/${start}/${start}/tmp/{z}/{x}/{y}.png`
+        ],
+        tileSize: 256,
+        scheme: 'tms'
     });
 
-    L.tileLayer(`${TILES_URL}/styles/osm-bright/256/{z}/{x}/{y}.png`, {
-        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-        zIndex: 19,
-    }).addTo(map);
-
-    L.timeDimension.velocityLayer({
-        baseURL: `${SERVER_URL}/layers/${start}`,
-        velocityLayerOptions: {
-            velocityScale: 0.01,
-            colorScale: ['#888'],
-        }
-    }).addTo(map);
-
-    var tileLayers = {}
-    Object.entries(layers).forEach(([name, layer]) => {
-        tileLayers[name] = L.timeDimension.timeLayer(
-            L.tileLayer(`${SERVER_URL}/layers/${start}/{d}/${layer}/{z}/{x}/{y}.png`, {
-                tms: true,
-            })
-        );
-    })
-
-    var defaultLayer = 'Temperature';
-
-    tileLayers[defaultLayer].addTo(map)
-
-    L.control.legend({
-        baseUrl: `${SERVER_URL}/layers`,
-        defaultLayer,
-    }).addTo(map);
-
-    var layersControl = L.control.layers(tileLayers, []);
-    layersControl.addTo(map);
+    // Add a layer for your custom tile source
+    map.addLayer({
+        'id': 'custom-tiles-layer',
+        'type': 'raster',
+        'source': 'custom-tiles',
+        'paint': {}
+    });
 }
 
 let lastStartDatetime;
@@ -113,13 +66,107 @@ async function init() {
     }
 }
 
-document.addEventListener('DOMContentLoaded', () => {
-    document.addEventListener('visibilitychange', () => {
-        if (document.visibilityState === 'visible') {
-            console.log('Reloading')
-            init();
-        }
-    });
-
+map.on('load', () => {
     init();
 });
+
+// document.addEventListener('DOMContentLoaded', () => {
+//     document.addEventListener('visibilitychange', () => {
+//         if (document.visibilityState === 'visible') {
+//             console.log('Reloading')
+//             init();
+//         }
+//     });
+
+//     init();
+// });
+
+
+// import 'leaflet/dist/leaflet.css';
+// import 'leaflet-velocity/dist/leaflet-velocity.css';
+// import 'leaflet-timedimension/dist/leaflet.timedimension.control.css';
+
+// import L from 'leaflet';
+// import 'leaflet-velocity/dist/leaflet-velocity.js';
+// import 'leaflet-timedimension/dist/leaflet.timedimension.src.js';
+// import './src/TimeLayer.js';
+// import './src/TimeVelocityLayer.js';
+// import './src/legend.js';
+// import { layers } from './src/layers.js';
+
+
+
+// let map;
+
+// function render(startDatetime, numForecasts) {
+//     let hourLimit = (numForecasts - 1) * hoursPerForecast;
+//     let start = (new Date(startDatetime)).toISOString()
+//                                              .substring(0, 13);
+
+//     if (map) {
+//         map.off();
+//         map.remove()
+//     }
+    
+//     map = L.map('map', {
+//         center: [20, -10],
+//         zoom: 3,
+//         minZoom: 3,
+//         maxZoom: 6,
+//         zoomSnap: 0.5,
+//         zoomDelta: 1,
+//         maxBounds: [[-85, -720], [85, 720]],
+//         timeDimension: true,
+//         timeDimensionOptions: {
+//             timeInterval: `${startDatetime}/PT${hourLimit}H`,
+//             period: `PT${hoursPerForecast}H`,
+//         },
+//         timeDimensionControl: true,
+//         timeDimensionControlOptions: {
+//             playerOptions: {
+//                 transitionTime: 2000,
+//             },
+//             timeZones: ["Local"],
+//             playButton: true,
+//             loopButton: true,
+//             speedSlider: false,
+//         },
+//     });
+
+//     L.tileLayer(`${TILES_URL}/styles/osm-bright/256/{z}/{x}/{y}.png`, {
+//         attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+//         zIndex: 19,
+//     }).addTo(map);
+
+//     L.timeDimension.velocityLayer({
+//         baseURL: `${SERVER_URL}/layers/${start}`,
+//         velocityLayerOptions: {
+//             velocityScale: 0.01,
+//             colorScale: ['#888'],
+//         }
+//     }).addTo(map);
+
+//     var tileLayers = {}
+//     Object.entries(layers).forEach(([name, layer]) => {
+//         tileLayers[name] = L.timeDimension.timeLayer(
+//             L.tileLayer(`${SERVER_URL}/layers/${start}/{d}/${layer}/{z}/{x}/{y}.png`, {
+//                 tms: true,
+//             })
+//         );
+//     })
+
+//     var defaultLayer = 'Temperature';
+
+//     tileLayers[defaultLayer].addTo(map)
+
+//     L.control.legend({
+//         baseUrl: `${SERVER_URL}/layers`,
+//         defaultLayer,
+//     }).addTo(map);
+
+//     var layersControl = L.control.layers(tileLayers, []);
+//     layersControl.addTo(map);
+// }
+
+
+
